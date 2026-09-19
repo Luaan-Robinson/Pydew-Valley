1e4da8677d6eaf0443c99a264743f81553a63c83
Luaan-Robinson <106935625+Luaan-Robinson@users.noreply.github.com>
2026-09-19 11:11:30 +0200

    player movement animations

--- code/player.py
+++ code/player.py
@@ -30,25 +30,38 @@ class Player(pygame.sprite.Sprite):
      for animation in self.animations.keys():
         full_path = './graphics/character/' + animation
         self.animations[animation] = import_folder(full_path)
-     print(self.animations)   
+
+  def animate(self, dt):
+     self.frame_index += 4 * dt
+     if self.frame_index >= len(self.animations[self.status]): # helps prevent the frame index from going out of bounds
+        self.frame_index = 0
+     self.image = self.animations[self.status][int(self.frame_index)] # look here man, I'm already getting lost
 
   def input(self):
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_UP]:
         self.direction.y = -1
+        self.status = 'up'
     elif keys[pygame.K_DOWN]:
         self.direction.y = 1
+        self.status = 'down'
     else:
        self.direction.y = 0    
 
     if keys[pygame.K_RIGHT]:
         self.direction.x = 1
+        self.status = 'right'
     elif keys[pygame.K_LEFT]:
-          self.direction.x = -1        
+          self.direction.x = -1  
+          self.status = 'left'      
     else:
        self.direction.x = 0    
-       
+
+  def get_status(self):
+     if  self.direction.magnitude() == 0:
+        self.status = self.status.split('_')[0] + '_idle' # the first item returned from this list will always be the status
+
   def move(self, dt):
 
     # normalizing a vector
@@ -64,4 +77,6 @@ class Player(pygame.sprite.Sprite):
 
   def update(self, dt):
     self.input()
+    self.get_status()
     self.move(dt)
+    self.animate(dt)
